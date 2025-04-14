d31f9ed6fefcc58f8f70091bc4b40a9da0c64c6c
robertgrc <robert.rodriguez.rgrc@gmail.com>
2025-04-14 13:18:34 -0400

    modificando el dashboard

--- src/app/(routes)/(dashboard)/dashboard/page.tsx
+++ src/app/(routes)/(dashboard)/dashboard/page.tsx
@@ -2,6 +2,15 @@
 
 export default function DashboardPage() {
   return (
-    <div>DashboardPage</div>
+    <div>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">
+          List of Cars
+        </h2>
+      </div>
+      <div>
+        <p>Listado de coches</p>
+      </div>
+    </div>
   )
 }
